afd7f4b55d909026d1dc86ac04f6c852cf699250
djfan <df1676@nyu.edu>
2020-07-13 22:37:33 -0400

    Updates

---
+++ precache-manifest.994c2ce7f945f54694ac7830f4895711.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bf71c0c2a97ef96062d85131d3134a0",
+    "url": "/country_info/index.html"
+  },
+  {
+    "revision": "9f787728245be3a58ad6",
+    "url": "/country_info/static/js/2.69bc799d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/country_info/static/js/2.69bc799d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "549a64b90540500ffcb8",
+    "url": "/country_info/static/js/main.9af5eff0.chunk.js"
+  },
+  {
+    "revision": "1944224273e196680232",
+    "url": "/country_info/static/js/runtime-main.65a22565.js"
+  }
+]);
